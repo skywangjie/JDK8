86fc65f24f40679a53f2f556cb3d78fb67e92e50
WJJJ <244731110@qq.com>
2019-07-12 07:31:20 +0800

    JDK8特性 demo

---
+++ src/main/java/com/gradle/wjjj/stream/Student.java
@@ -0,0 +1,46 @@
+package com.gradle.wjjj.stream;
+
+import lombok.Data;
+
+/**
+ *@author WJJJ
+ *@createTime 2019/7/11 19:58
+ *@description
+ */
+
+@Data
+public class Student {
+    private String name;
+    private int score;
+    private  int age;
+
+    public Student(String name, int score, int age) {
+        this.name = name;
+        this.score = score;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
